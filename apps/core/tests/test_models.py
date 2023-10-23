6c745a2c53ab34ffaae4b1ce578c36df6033ccd9
kamila2508 <kamila3602@gmail.com>
2023-10-23 12:29:27 +0200

    Add unit tests for CustomUser model

---
+++ apps/core/tests/test_models.py
@@ -0,0 +1,36 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+# CustomUser model tests
+@pytest.mark.django_db
+def test_create_user():
+    email = 'test@example.com'
+    username = 'testuser'
+    password = 'testpass123'
+    user = User.objects.create_user(
+        email=email,
+        username=username,
+        password=password,
+    )
+    assert user.email == email
+    assert user.username == username
+    assert user.check_password(password) is True
+    assert user.is_active is True
+    assert user.is_staff is False
+
+
+@pytest.mark.django_db
+def test_create_superuser():
+    email = 'admin@example.com'
+    username = 'testadmin'
+    password = 'testsuperpass123'
+    admin = User.objects.create_superuser(
+        email=email,
+        username=username,
+        password=password,
+    )
+    assert admin.is_superuser is True
+    assert admin.is_staff is True
