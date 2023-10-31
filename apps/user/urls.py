9471a30ee59e84837f5255447c0dcd80a0b03486
kamila2508 <kamila3602@gmail.com>
2023-10-31 11:14:32 +0100

    Create UserLoginView and login template

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,17 +1,18 @@
 """
 URLs for user app.
 """
-
 from django.urls import path
 
 from .views import (
-    RegistrationView,
     home_page,
+    RegistrationView,
+    UserLoginView
 )
 
 app_name = 'user'
 
 urlpatterns = [
+    path('home/', home_page, name='home-page'),
     path('register/', RegistrationView.as_view(), name='register'),
-    path('', home_page, name='home-page')
+    path('login/', UserLoginView.as_view(), name='login')
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate, login
+
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+
 from django.views import View
 from django.views.generic import CreateView
 
+from .forms import UserLoginForm
 from ..core.forms import CustomUserCreationForm
+
 
 User = get_user_model()
 
@@ -29,3 +33,23 @@ class RegistrationView(CreateView):
         user = authenticate(email=email, password=password)
         login(self.request, user)
         return redirect(self.get_success_url())
+
+
+class UserLoginView(View):
+    template_name = 'user/login.html'
+
+    def get(self, request):
+        form = UserLoginForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse_lazy('user:home-page'))
+
+        return render(request, self.template_name, {'form': form})
