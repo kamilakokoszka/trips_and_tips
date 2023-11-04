2c99e9dae1886b5a42df0fb5177e86defa33d06c
kamila2508 <kamila3602@gmail.com>
2023-11-04 15:55:41 +0100

    Create Profile model

--- apps/core/models.py
+++ apps/core/models.py
@@ -41,7 +41,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     class Meta:
@@ -50,3 +49,17 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    website = models.CharField(max_length=255, blank=True, null=True)
+    picture = models.ImageField(upload_to='images/user/')
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        return f"{self.user}'s profile"
