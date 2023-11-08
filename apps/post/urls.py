dc79d6b4ef6108db2149f3330d4334cbd1da12ad
kamila2508 <kamila3602@gmail.com>
2023-11-08 13:30:01 +0100

    Create PostListView and PostDetailView

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -2,7 +2,14 @@
 URLs for post app.
 """
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+)
 
 app_name = 'post'
 
-urlpatterns = []
+urlpatterns = [
+    path('', PostListView.as_view(), name='list'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='details')
+]
--- apps/post/views.py
+++ apps/post/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+
+from django.views import View
+from django.views.generic import (
+    CreateView,
+    TemplateView,
+    DeleteView,
+    ListView,
+    DetailView,
+)
+
+from apps.core.models import Post
+
+
+User = get_user_model()
+
+
+# ------- Post views ------- #
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'post/list.html'
+    paginate_by = 5
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/details.html'
+
