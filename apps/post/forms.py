5298f2155dddd024bf11b1473bf8777982dbe3c6
kamila2508 <kamila3602@gmail.com>
2023-11-17 14:25:10 +0100

    Create CommentForm

--- apps/post/forms.py
+++ apps/post/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from apps.core.models import Post
+from apps.core.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -8,3 +8,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'slug', 'body', 'image', 'tags']
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['body']
