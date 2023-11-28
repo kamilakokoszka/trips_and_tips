04db473b290480e09ac28721d603ae6a0240de06
kamila2508 <kamila3602@gmail.com>
2023-11-28 19:37:50 +0100

    Add STATICFILES_DIRS and STATIC_URL

--- trips_and_tips/settings.py
+++ trips_and_tips/settings.py
@@ -147,3 +147,9 @@ LOGIN_URL = '/user/login/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_URL = '/static/'
